0f3e3d3e5a0fbdbb3444a319d4316a4a83534748
Rohan Gupta <rohangupta30128@gmail.com>
2024-09-21 12:32:34 +0530

    Setup Frontend with the help of react and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+       <h1 className="text-3xl font-bold underline bg-purple-500 text-white">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
