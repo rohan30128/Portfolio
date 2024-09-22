d9ac3e0854dae3903e7486330eea1faba45e1870
Rohan Gupta <rohangupta30128@gmail.com>
2024-09-22 19:54:07 +0530

    Form validation complete on frontend with the help of react-hook-form

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import Sectiontitle from "../Sectiontitle";
+import { useForm } from "react-hook-form";
 
 export default function Contact() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
+  console.log(watch("example"));
   return (
     <div>
-      <Sectiontitle title="Say Hello"id="contact" />
+      <Sectiontitle title="Say Hello" id="contact" />
       <div className="sm:w-1/2 w-80 mx-auto">
-        <form action="">
+        <form action="" onSubmit={handleSubmit(onSubmit)}>
           <div className="my-5">
             <label className="input input-bordered flex items-center gap-2">
               <svg
@@ -17,7 +28,8 @@ export default function Contact() {
               >
                 <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
               </svg>
-              <input type="text" className="grow" placeholder="Full Name" />
+              <input {...register("name", { required: true })} type="text" className="grow" placeholder="Full Name" />
+              {errors.name && <span>This field is required</span>}
             </label>
           </div>
           <div className="my-5">
@@ -31,7 +43,8 @@ export default function Contact() {
                 <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
                 <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
               </svg>
-              <input type="text" className="grow" placeholder="Email" />
+              <input {...register("email", { required: true })} type="text" className="grow" placeholder="Email" />
+              {errors.email && <span>This field is required</span>}
             </label>
           </div>
           <div className="my-5">
@@ -44,18 +57,31 @@ export default function Contact() {
               >
                 <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
               </svg>
-              <input type="text" className="grow" placeholder="Phone Number" />
+              <input {...register("number", { required: true })} type="text" className="grow" placeholder="Phone Number" />
+              {errors.number && <span>This field is required</span>}
             </label>
           </div>
           <div className="my-5">
-          <input type="text" placeholder="Subject" className="input input-bordered w-full " />
+            <label className="input input-bordered flex items-center gap-2">
+            <input
+            {...register("subject", { required: true })}
+              type="text"
+              placeholder="Subject"
+              className="input  w-full "
+            />
+            {errors.subject && <span>This field is required</span>}
+            </label>
           </div>
           <div className="my-5">
-          <textarea
-  placeholder="Message"
-  className="textarea textarea-bordered textarea-lg w-full "></textarea>
+            <textarea
+              placeholder="Message"
+              className="textarea textarea-bordered textarea-lg w-full "
+            ></textarea>
           </div>
-          <div><button className="btn btn-accent" type="submit">Submit</button></div>
+          <div>
+            
+            <input type="submit" className="btn btn-accent" />
+          </div>
         </form>
       </div>
     </div>
